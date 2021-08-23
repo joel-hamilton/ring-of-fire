bb58647e90aba751fea50337759d489d3c53c765
Joel Hamilton <joelhamilton5@gmail.com>
2021-08-23 11:12:48 -0400

    setup map

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Info from './components/Info';
+import Map from './components/Map';
+import Settings from './components/Settings';
+import { DateTime } from 'luxon';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [settings, setSettings] = useState({ start: DateTime.now().minus({weeks: 2}).toISO(), end: DateTime.now().toISO(), magnitude: 5, speed: 5 });
+    useEffect(() => {
+        async function load() {
+            await loadData();
+        }
+
+        load();
+    }, []);
+
+
+    const updateSettings = function (key: string, value: string) {
+        if(key === 'start' || key == 'end') {
+            const date:DateTime = DateTime.fromISO(value);
+            if(!date.isValid) return;
+            value = date.toISO();
+        }
+        setSettings({ ...settings, [key]: value });
+    }
+
+    const loadData = async function () {
+        //
+    }
+
+    return (
+        <div className="app">
+            {`loading data from: ${settings.start} to ${settings.end}, magnitude >${settings.magnitude}`}
+            <Info settings={settings} />
+            <Map />
+            <Settings settings={settings} onUpdate={updateSettings} />
+        </div>
+    );
 }
 
 export default App;
